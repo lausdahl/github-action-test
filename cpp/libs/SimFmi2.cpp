08f4f93bfe00d90abc64a39c548e1389a9877aff
Kenneth Lausdahl <Kenneth.GuldbrandtLausdahl@agcocorp.com>
2021-07-22 13:05:00 +0200

    test

--- cpp/libs/SimFmi2.cpp
+++ cpp/libs/SimFmi2.cpp
@@ -15,6 +15,34 @@ extern "C" {
 }
 
 
+#include <system_error>
+#include <memory>
+#include <string>
+std::string GetLastErrorAsString()
+{
+    //Get the error message ID, if any.
+    DWORD errorMessageID = ::GetLastError();
+    if(errorMessageID == 0) {
+        return std::string(); //No error message has been recorded
+    }
+    
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+    
+    //Copy the error message into a std::string.
+    std::string message(messageBuffer, size);
+    
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+            
+    return message;
+}
+
+
 bool hasEnding (std::string const &fullString, std::string const &ending) {
     if (fullString.length() >= ending.length()) {
         return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
@@ -113,6 +141,12 @@ FMI2 load_FMI2(const char *guid, const char *path) {
 
     fmu->guid = guid;
     auto success = loadDll(fmu->library_path.c_str(), &fmu->fmu);
+
+
+if(!success){
+std::cout << GetLastErrorAsString()<<std::endl;
+}
+
     auto t2 = std::chrono::high_resolution_clock::now();
 
     auto dur = t2 - t1;
